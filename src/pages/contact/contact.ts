9a12bedd50e69e83778998e4ab66733a07079731
O3Ob <pushupclub312@gmail.com>
2018-03-30 19:10:12 +0800

    [UPDATE] Practice with ListView

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DetailPage } from '../detail/detail';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-
+  items: any[];
+  
   constructor(public navCtrl: NavController) {
-
+    this.items = [];
+    for(let i = 0; i < 10; i++ ){
+      this.items.push({
+        text: 'OAO '+ i,
+        id: i
+      });
+    }
   }
 
+  itemSelected(item){
+    // alert(item.text);
+    this.navCtrl.push(DetailPage, {
+      item: item
+    });
+  }
 }
